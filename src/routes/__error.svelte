b47bc549be110cd5ae475678d7cce3b6807d2e47
Tomi Chen <tctree333@users.noreply.github.com>
2022-01-06 20:58:10 -0800

    init new project

---
+++ src/routes/__error.svelte
@@ -0,0 +1,22 @@
+<script context="module" lang="ts">
+	import type { ErrorLoad } from '@sveltejs/kit';
+
+	export const load: ErrorLoad = ({ error, status }) => {
+		return {
+			props: {
+				title: `${status}: ${error.message}`,
+				...error
+			}
+		};
+	};
+</script>
+
+<script lang="ts">
+	export let title: string;
+	export let stack: string;
+	export let name: string;
+</script>
+
+<h1>{title}</h1>
+<h2>{name}</h2>
+<pre>{stack}</pre>
