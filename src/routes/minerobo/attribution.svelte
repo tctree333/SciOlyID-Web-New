d36ba5a5a4d8bb89ab697cf8594de9d25d80ff6a
Tomi Chen <tctree333@users.noreply.github.com>
2022-01-08 14:15:10 -0800

    add attributions page

---
+++ src/routes/minerobo/attribution.svelte
@@ -0,0 +1,47 @@
+<script context="module" lang="ts">
+	import type { Load } from '@sveltejs/kit';
+
+	export const load: Load = async ({ params, fetch }) => {
+		const url =
+			'https://raw.githubusercontent.com/tctree333/SciOlyID-Web/master/src/_data/minerobo_attributions.json';
+		if (!url) return { status: 404, message: 'Not found' };
+
+		const res = await fetch(url);
+
+		if (res.ok) {
+			return {
+				props: { attribution: await res.json() }
+			};
+		} else {
+			return {
+				status: res.status,
+				error: new Error(`Could not load ${url}: ${res.status} ${res.statusText}`)
+			};
+		}
+	};
+
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+	import Head from '$lib/components/Head.svelte';
+	import MarkdownIt from 'markdown-it';
+	const md = new MarkdownIt();
+
+	export let attribution: { [hash: string]: string };
+
+	const renderedAttribs = Object.values(attribution).map((attrib) => md.renderInline(attrib));
+</script>
+
+<Head title="Image Attributions: Minerobo | SciOlyID" description="" />
+
+<h1>Image Attributions: Minerobo | SciOlyID</h1>
+
+<ul>
+	<li>
+		Images from <a href="https://www.minerals.net">Minerals.net</a> are licensed for educational use.
+	</li>
+	{#each renderedAttribs as attrib}
+		<li>{@html attrib}</li>
+	{/each}
+</ul>
