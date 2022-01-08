bf418f1491f530f05e63d734f84d97e1e897977c
Tomi Chen <tctree333@users.noreply.github.com>
2022-01-08 13:41:06 -0800

    trigger 404 on invalid routes

---
+++ src/routes/[bot]/__layout.svelte
@@ -0,0 +1,20 @@
+<script context="module" lang="ts">
+	import config from '$lib/config';
+
+	import type { Load } from '@sveltejs/kit';
+
+	export const load: Load = async ({ params }) => {
+		if (Object.keys(config.bots).includes(params.bot)) {
+			return {
+				status: 200
+			};
+		} else {
+			return {
+				status: 404,
+				error: 'Not found'
+			};
+		}
+	};
+</script>
+
+<slot />
---
+++ src/routes/[bot]/index.svelte
@@ -0,0 +1,10 @@
+<script context="module" lang="ts">
+	import type { Load } from '@sveltejs/kit';
+
+	export const load: Load = async () => {
+		return {
+			status: 404,
+			error: 'Not Found'
+		};
+	};
+</script>
