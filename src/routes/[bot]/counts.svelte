87c2d085cd5bdf50568c51f3289d1943df2dbd5f
Tomi Chen <tctree333@users.noreply.github.com>
2022-01-10 14:40:30 -0800

    style counts page

--- src/routes/[bot]/counts.svelte
+++ src/routes/[bot]/counts.svelte
@@ -35,25 +35,43 @@
 		$page.params.bot
 	].eventName}"
 />
+<main class="prose prose-lg md:prose-xl mx-auto px-8 mt-36 mb-24 prose-stone">
+	<h1>
+		Image Counts: {config.bots[$page.params.bot].botName}
+	</h1>
 
-<h1>
-	Image Counts: {config.bots[$page.params.bot].botName}
-</h1>
+	<p class="font-medium">Total Images: {counts.total}</p>
+	<ul class="my-4 not-prose list-none">
+		{#each counts.counts as item}
+			{#if item.group}
+				<li class="mb-3 font-semibold">
+					<span class="text-2xl">{item.name}</span>
+					<svg
+						class="inline w-6 h-6 text-stone-700 fill-current relative bottom-[0.14rem] right-[0.2rem]"
+						width="24"
+						height="24"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 20 20"
+					>
+						<path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+					</svg>
+					<ul class="flex flex-row flex-wrap items-center mb-8 font-normal list-inside item-list">
+						{#each item.value as subitem}
+							<li class="mr-4 my-2">
+								{subitem.name}: <span class="font-medium">{subitem.value}</span>
+							</li>
+						{/each}
+					</ul>
+				</li>
+			{:else}
+				<li>{item.name}: {item.value}</li>
+			{/if}
+		{/each}
+	</ul>
+</main>
 
-<p>Total Images: {counts.total}</p>
-<ul>
-	{#each counts.counts as item}
-		{#if item.group}
-			<li>
-				<span>{item.name}</span>
-				<ul>
-					{#each item.value as subitem}
-						<li>{subitem.name}: {subitem.value}</li>
-					{/each}
-				</ul>
-			</li>
-		{:else}
-			<li>{item.name}: {item.value}</li>
-		{/if}
-	{/each}
-</ul>
+<style>
+	ul.not-prose {
+		padding: 0;
+	}
+</style>
