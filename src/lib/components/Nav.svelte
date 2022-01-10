a0e7b5a006f97a83e243974addb824a046e9e931
Tomi Chen <tctree333@users.noreply.github.com>
2022-01-10 13:01:06 -0800

    add nav styles

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,17 +1,83 @@
 <script lang="ts">
+	import { browser } from '$app/env';
+	import { navigating } from '$app/stores';
+
 	import config from '$lib/config';
+	import NavMenu from './NavMenu.svelte';
 	import UserButton from './UserButton.svelte';
+
+	let showMenu = false;
+
+	$: {
+		if (browser) {
+			if (showMenu) {
+				document.body.classList.add('overflow-hidden', 'h-full');
+			} else {
+				document.body.classList.remove('overflow-hidden', 'h-full');
+			}
+		}
+	}
+
+	navigating.subscribe(() => {
+		showMenu = false;
+	});
 </script>
 
-<nav>
-	<ul>
-		<li><a sveltekit:prefetch href="/">SciOlyID</a></li>
-		<li><a sveltekit:prefetch href="/about/">About</a></li>
-		<li><a sveltekit:prefetch href="/guides/">Guides</a></li>
-		{#each Object.entries(config.bots) as [id, bot]}
-			<li>
-				<ul>
-					<span>{bot.eventName}</span>
+<svelte:body
+	on:keypress={(ev) => {
+		if (ev.key === 'Escape') {
+			showMenu = false;
+		}
+	}} />
+
+<nav
+	class="md:px-12 md:py-8 py-6 px-8 text-xl fixed top-0 inset-x-0 bg-white bg-opacity-90"
+	class:opened={showMenu}
+>
+	<div class="flex justify-between max-w-7xl mx-auto items-center">
+		<button
+			aria-hidden="true"
+			class="block md:hidden z-10 md:z-auto"
+			on:click={() => {
+				showMenu = !showMenu;
+			}}
+		>
+			{#if showMenu}<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M6 18L18 6M6 6l12 12"
+					/>
+				</svg>{:else}<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M4 6h16M4 12h16M4 18h16"
+					/>
+				</svg>{/if}</button
+		>
+		<a sveltekit:prefetch href="/" class="text-3xl md:z-auto z-10">SciOlyID</a>
+		<span class="links flex md:space-x-6 space-y-4 md:space-y-0 md:z-auto z-0 underline">
+			<a sveltekit:prefetch href="/about/">About</a>
+			<a sveltekit:prefetch href="/guides/">Guides</a>
+			{#each Object.entries(config.bots) as [id, bot]}
+				<NavMenu title={bot.eventName}>
 					{#each Object.entries(config.sitePaths) as [path, info]}
 						{#if bot[path]}
 							<li>
@@ -19,9 +85,50 @@
 							</li>
 						{/if}
 					{/each}
-				</ul>
-			</li>
-		{/each}
-		<li><UserButton /></li>
-	</ul>
+				</NavMenu>
+			{/each}
+		</span>
+		<span class="md:z-auto z-10">
+			<UserButton />
+		</span>
+	</div>
 </nav>
+
+<style>
+	/* based on https://bholmes.dev/blog/building-a-sexy-mobile-ready-navbar-in-any-web-framework/ */
+	@media (max-width: 767.9px) {
+		/* arbitrary breakpoint, around the size of a tablet */
+		.links {
+			/* first, make our dropdown cover the screen */
+			position: fixed;
+			top: 0;
+			left: 0;
+			right: 0;
+			height: 100vh;
+			/* fix nav height on mobile safari, where 100vh is a little off */
+			height: -webkit-fill-available;
+
+			/* then, arrange our links top to bottom */
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: flex-start;
+
+			/* add margins and padding to taste */
+			margin: 0;
+			padding-left: 7vw;
+			padding-right: 7vw;
+
+			background: hsla(0deg, 100%, 100%, 90%);
+
+			opacity: 0; /* fade out */
+			transform: translateY(-100%); /* move out of view */
+			transition: transform 0.2s, opacity 0.2s; /* transition these smoothly */
+		}
+
+		nav.opened .links {
+			opacity: 1; /* fade in */
+			transform: translateY(0); /* move into view */
+		}
+	}
+</style>
---
+++ src/lib/components/NavMenu.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import { tweened } from 'svelte/motion';
+
+	export let title: string;
+
+	let show = false;
+
+	let id = 'navMenu-' + Math.round(Math.random() * 1000);
+
+	const opacity = tweened(0, { duration: 200 });
+
+	$: {
+		show ? ($opacity = 1) : ($opacity = 0);
+	}
+</script>
+
+<svelte:body
+	on:click={(ev) => {
+		if (show && !ev.target.closest('#' + id)) show = false;
+	}} />
+
+<span {id} class="group">
+	<button
+		class="hidden md:block"
+		on:click={() => {
+			show = !show;
+		}}>{title}</button
+	>
+	<span class="block no-underline md:hidden">{title}</span>
+	<ul
+		style="--opacity: {$opacity};"
+		class="md:list-none list-disc ml-8 md:ml-0 space-y-1 underline {show
+			? 'md:block'
+			: 'md:hidden'} md:absolute md:bg-slate-200 md:mt-2 md:p-4 md:rounded-sm opacity-100"
+	>
+		<slot />
+	</ul>
+</span>
+
+<style>
+	@media (min-width: 768px) {
+		ul {
+			opacity: var(--opacity);
+		}
+	}
+</style>
