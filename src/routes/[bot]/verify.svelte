0c130317417155c22287dd10be5ac1d3c8253a9a
Tomi Chen <tctree333@users.noreply.github.com>
2022-01-10 15:54:36 -0800

    style verify page

--- src/routes/[bot]/verify.svelte
+++ src/routes/[bot]/verify.svelte
@@ -145,75 +145,86 @@
 		.eventName} images to ensure image quality and accuracy."
 />
 
-<h1>
-	Verify Images: {config.bots[$page.params.bot].botName}
-</h1>
+<main class="main">
+	<div class="prose-content">
+		<h1 class="text-center">
+			Verify Images: {config.bots[$page.params.bot].botName}
+		</h1>
+	</div>
 
-{#if !hidden}
-	<div>
-		<p>
-			Is this an image of <span>{loading ? '...' : item.item}</span>?
-		</p>
-		<div>
-			<p>
-				Image ID: <span>{loading ? '...' : item.id.slice(0, 7)}</span>
+	{#if !hidden}
+		<div class="container max-w-4xl px-5 py-4 mx-auto mb-4 sm:px-8">
+			<p class="text-xl">
+				Is this an image of <span class="font-semibold">{loading ? '...' : item.item}</span>?
 			</p>
-			<div>
-				<p>
-					{loading ? 0 : stats.valid}
+			<div class="flex items-center justify-between mt-4 mb-2">
+				<p class="font-semibold">
+					Image ID: <span class="font-normal">{loading ? '...' : item.id.slice(0, 7)}</span>
 				</p>
-				<p>
-					{loading ? 0 : stats.duplicate}
-				</p>
-				<p>
-					{loading ? 0 : stats.invalid}
-				</p>
+				<div class="flex items-center justify-end">
+					<p class="px-2 mr-2 text-green-800 bg-green-300 rounded-full">
+						{loading ? 0 : stats.valid}
+					</p>
+					<p class="px-2 mr-2 text-yellow-800 bg-yellow-300 rounded-full">
+						{loading ? 0 : stats.duplicate}
+					</p>
+					<p class="px-2 mr-2 text-red-800 bg-red-300 rounded-full">
+						{loading ? 0 : stats.invalid}
+					</p>
+				</div>
 			</div>
-		</div>
-		<div>
 			<div>
-				<img
-					src={loading ? '/illustrations/loading.svg' : baseUrl + item.url}
-					alt="possible specimen"
-				/>
+				<div class="p-4 py-8 my-4 bg-neutral-200 rounded">
+					<img
+						src={loading ? '/illustrations/loading.svg' : baseUrl + item.url}
+						class="mx-auto rounded max-h-72"
+						alt="possible specimen"
+					/>
+				</div>
+
+				{#if !loading && item.duplicates}
+					<div
+						class="p-4 pt-2 mx-auto my-4 mb-2 bg-orange-200 border-t-4 border-yellow-700 rounded"
+					>
+						<p>Possible Duplicates:</p>
+						{#each item.duplicates as duplicate}
+							<img src={duplicate} alt="a possible duplicate" />
+						{/each}
+					</div>
+				{/if}
 			</div>
 
-			{#if !loading && item.duplicates}
-				<div>
-					<p>Possible Duplicates:</p>
-					{#each item.duplicates as duplicate}
-						<img src={duplicate} alt="a possible duplicate" />
-					{/each}
-				</div>
-			{/if}
-		</div>
-		<div>
-			<div>
+			<div class="flex flex-wrap items-center justify-center space-x-4 text-lg mt-8">
 				<button
+					class="btn btn-gray"
 					on:click={() => {
 						updateImage(offset() - 1);
 					}}
 					disabled={loading}>Back</button
 				>
 				<button
+					class="btn btn-green"
 					on:click={() => {
 						genValidation('valid');
 					}}
 					disabled={loading}>Valid</button
 				>
 				<button
+					class="btn btn-yellow"
 					on:click={() => {
 						genValidation('duplicate');
 					}}
 					disabled={loading}>Duplicate</button
 				>
 				<button
+					class="btn btn-red"
 					on:click={() => {
 						genValidation('invalid');
 					}}
 					disabled={loading}>Invalid</button
 				>
 				<button
+					class="btn btn-gray"
 					on:click={() => {
 						updateImage();
 					}}
@@ -221,35 +232,43 @@
 				>
 			</div>
 		</div>
+	{:else}
+		<p>No images to verify!</p>
+	{/if}
+
+	<div
+		class="container max-w-4xl p-5 mx-auto my-16 ring-4 ring-stone-400 rounded shadow-sm sm:px-8"
+	>
+		<div class="max-w-3xl mx-auto">
+			<p class="mb-3 text-xl font-medium">
+				Help maintain the quality of images used by the bot by verifying user-uploaded images.
+			</p>
+			<div class="mb-3">
+				<p class="mb-3 font-semibold">You can mark an image as:</p>
+				<ul class="ml-2 leading-relaxed list-disc list-inside">
+					<li class="mb-2">
+						<span class="p-1 px-2 font-medium text-green-900 bg-green-300 rounded-lg">Valid</span> -
+						The object to be identified is clearly visible and is of the correct item. Text revealing
+						the answer is not visible. The image is not a duplicate of an already existing image.
+					</li>
+					<li class="mb-2">
+						<span class="p-1 px-2 font-medium text-yellow-900 bg-yellow-300 rounded-lg"
+							>Duplicate</span
+						> - The image is a duplicate of an already existing image. You will be presented with likely
+						duplicates by the bot if they exist; only mark as duplicate if they are the same image.
+					</li>
+					<li class="mb-2">
+						<span class="p-1 px-2 font-medium text-red-900 bg-red-300 rounded-lg">Invalid</span> - The
+						object to be identified is not clearly visible or is incorrect, or spoiler text is visible.
+						If a person is prominently featured or identifiable, also mark that as invalid.
+					</li>
+				</ul>
+			</div>
+			<p>
+				Verification counts are checked every 15 minutes. If an image has at least 3 votes for
+				invalid or duplicate, the image is discarded, otherwise if there are at least 3 votes for
+				valid, then it will be added to the bot.
+			</p>
+		</div>
 	</div>
-{:else}
-	<p>No images to verify!</p>
-{/if}
-<div>
-	<div>
-		<p>Help maintain the quality of images used by the bot by verifying user-uploaded images.</p>
-		<p>You can mark an image as:</p>
-		<ul>
-			<li>
-				<span>Valid</span> - The object to be identified is clearly visible and is of the correct item.
-				Text revealing the answer is not visible. The image is not a duplicate of an already existing
-				image.
-			</li>
-			<li>
-				<span>Duplicate</span> - The image is a duplicate of an already existing image. You will be presented
-				with likely duplicates by the bot if they exist; only mark as duplicate if they are the same
-				image.
-			</li>
-			<li>
-				<span>Invalid</span> - The object to be identified is not clearly visible or is incorrect, or
-				spoiler text is visible. If a person is prominently featured or identifiable, also mark that
-				as invalid.
-			</li>
-		</ul>
-		<p>
-			Verification counts are checked every 15 minutes. If an image has at least 3 votes for invalid
-			or duplicate, the image is discarded, otherwise if there are at least 3 votes for valid, then
-			it will be added to the bot.
-		</p>
-	</div>
-</div>
+</main>
