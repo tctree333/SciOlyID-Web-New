974051d6051f60bab4567dc6496c503d719af1a8
Tomi Chen <tctree333@users.noreply.github.com>
2022-01-11 15:11:19 -0800

    style landing hero

--- src/lib/components/landing/Hero.svelte
+++ src/lib/components/landing/Hero.svelte
@@ -2,6 +2,36 @@
 	export let title: string;
 </script>
 
-<h1>{title}</h1>
-<slot name="description" />
-<slot name="cta" />
+<section class="w-full py-8 max-w-7xl mx-auto mb-8">
+	<div class="flex items-center flex-col md:flex-row">
+		<div class="flex-1 md:pl-6 lg:pl-14 md:mr-4 md:min-w-[400px]">
+			<h1 class="font-bold mb-10 leading-tight max-w-lg text-center md:text-left">{title}</h1>
+			<div
+				class="max-w-sm mb-6 leading-relaxed font-normal text-stone-700 md:mx-0 mx-auto text-center md:text-left"
+			>
+				<slot name="description" />
+			</div>
+			<span class="flex flex-wrap items-start justify-center md:justify-start">
+				<slot name="cta" />
+			</span>
+		</div>
+		<img
+			class="block md:w-1/2 md:min-w-[440px] justify-end mt-6 -mr-20 md:m-0 min-w-[373px]"
+			src="/illustrations/hero.png"
+			alt="a 3d model of a student sitting on the ground next to some plants and a pile of books. their hair floats above them with dna, planets, fruit, and other symbols orbiting around the hair."
+		/>
+	</div>
+</section>
+
+<style>
+	@media (min-width: 768px) {
+		h1 {
+			font-size: clamp(3.5rem, 7.7vw - 1.5rem, 5rem);
+		}
+	}
+	@media (max-width: 767px) {
+		h1 {
+			font-size: clamp(2.25rem, 4.5vw + 1.4rem, 3.5rem);
+		}
+	}
+</style>
