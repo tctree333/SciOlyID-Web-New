d7adf5f1697e3219f2b1f4e02e8b99f9210a2853
Tomi Chen <tctree333@users.noreply.github.com>
2022-01-13 11:25:55 -0800

    enable modals

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,12 @@
 <script>
 	import '../app.css';
+	import Modal from 'svelte-simple-modal';
 	import Footer from '$lib/components/Footer.svelte';
 	import Nav from '$lib/components/Nav.svelte';
 </script>
 
-<Nav />
-<slot />
-<Footer />
+<Modal>
+	<Nav />
+	<slot />
+	<Footer />
+</Modal>
