81ecfb20053f240d2825b15c98768008e03232d8
Tomi Chen <tctree333@users.noreply.github.com>
2022-01-13 11:28:17 -0800

    add modals to bot pages

--- src/routes/[bot]/__layout.reset.svelte
+++ src/routes/[bot]/__layout.reset.svelte
@@ -24,6 +24,7 @@
 
 <script lang="ts">
 	import Notifications from 'svelte-notifications';
+	import Modal from 'svelte-simple-modal';
 	import { setContext } from 'svelte';
 	import Nav from '$lib/components/Nav.svelte';
 	import Footer from '$lib/components/Footer.svelte';
@@ -32,8 +33,10 @@
 	setContext('botUrl', botUrl);
 </script>
 
-<Nav />
-<Notifications>
-	<slot />
-</Notifications>
-<Footer />
+<Modal>
+	<Nav />
+	<Notifications>
+		<slot />
+	</Notifications>
+	<Footer />
+</Modal>
--- src/routes/bird-id/__layout.reset.svelte
+++ src/routes/bird-id/__layout.reset.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Notifications from 'svelte-notifications';
+	import Modal from 'svelte-simple-modal';
 	import { setContext } from 'svelte';
 	import Nav from '$lib/components/Nav.svelte';
 	import Footer from '$lib/components/Footer.svelte';
@@ -8,8 +9,10 @@
 	setContext('botUrl', config.bots['bird-id'].baseUrl);
 </script>
 
-<Nav />
-<Notifications>
-	<slot />
-</Notifications>
-<Footer />
+<Modal>
+	<Nav />
+	<Notifications>
+		<slot />
+	</Notifications>
+	<Footer />
+</Modal>
